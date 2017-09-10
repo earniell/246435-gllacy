30c31bbd1da9702f86dc066add345ba57b65eb45
Alexey <30443006+earniell@users.noreply.github.com>
2017-09-10 22:44:20 +0300

    final

--- js/script.js
+++ js/script.js
@@ -1,33 +1,50 @@
-
-
-<script>
-
 var link = document.querySelector(".button-feedback-1");
 var popup = document.querySelector(".modal-feedback");
 var close = popup.querySelector(".close-button");
-var login = popup.querySelector("[name=feedback-name]");
-var form = popup.querySelector("feedback-form");
+var name = popup.querySelector("[name=feedback-name]");
+var form = popup.querySelector("form");
 var email = popup.querySelector("[name=feedback-email]");
 var message = popup.querySelector("[name=feedback-message]");
+var storage = localStorage.getItem("name");
+var body = document.querySelector("body");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("modal-show");
 
+  popup.classList.add("modal-show");
+  body.classList.add("fixed");
+      
+  if (storage) {
+    name.value = storage;
+    email.focus();
+  } else {
+    name.focus();
+  }
 });
 
 close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-    login.focus();
-  });
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+
+  body.classList.remove("fixed");
+  name.focus();
+});
 
 form.addEventListener("submit", function (evt) {
-    if (!login.value || !email.value || !message.value) {
-      evt.preventDefault();
-      console.log("Нужно заполнить все поля");
-    } else {
-      localStorage.setItem("login", login.value);
+  if (!name.value || !email.value || !message.value) {
+    evt.preventDefault();
+    popup.offsetWidth = popup.offsetWidth;
+  } else {
+    localStorage.setItem("name", name.value);
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+
+      body.classList.remove("fixed");
     }
-  });
-</script>
+  }
+});
