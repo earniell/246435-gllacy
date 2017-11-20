352fe09621edbb78a345f303946aaf58cc66f53a
Alexey <30443006+earniell@users.noreply.github.com>
2017-11-21 01:23:51 +0300

    доработка стилей

--- js/script.js
+++ js/script.js
@@ -7,12 +7,16 @@ var email = popup.querySelector("[name=feedback-email]");
 var message = popup.querySelector("[name=feedback-message]");
 var storage = localStorage.getItem("name");
 var body = document.querySelector("body");
+var blur = document.querySelector(".blur");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
 
   popup.classList.add("modal-show");
+  popup.classList.add("modal-window-fade");
   body.classList.add("fixed");
+  blur.style.display = 'block';
+
       
   if (storage) {
     name.value = storage;
@@ -25,15 +29,20 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-
+  popup.classList.remove("modal-error");
+  popup.classList.remove("modal-window-fade");
   body.classList.remove("fixed");
+  blur.style.display = 'none';
   name.focus();
 });
 
 form.addEventListener("submit", function (evt) {
   if (!name.value || !email.value || !message.value) {
     evt.preventDefault();
+    popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
+    popup.classList.remove("modal-window-fade");
+    popup.classList.add("modal-error");
   } else {
     localStorage.setItem("name", name.value);
   }
@@ -43,8 +52,10 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
-
+      popup.classList.remove("modal-window-fade");
+      popup.classList.remove("modal-error");
       body.classList.remove("fixed");
+      blur.style.display = 'none';
     }
   }
 });
